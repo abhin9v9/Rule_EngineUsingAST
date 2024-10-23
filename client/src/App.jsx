cad48b49d4970a53d8cc6d7f08cdc8a6706829bc
abhin9v9 <chaudhary.abhin9v@gmail.com>
2024-10-23 14:03:08 +0530

    UI changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import RuleEngine from "./components/RuleEngine";
 function App() {
   return (
     <div>
-      <h1>Rule Engine App</h1>
+      {/* <h1>Rule Engine App</h1> */}
       <RuleEngine />
     </div>
   );
